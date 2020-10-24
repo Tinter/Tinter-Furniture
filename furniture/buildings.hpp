b49fa7e5d6190d04c1d586f02e687ce2aa055f14
Tinter <a-348@hotmail.com>
2020-10-24 15:14:13 +0200

    Added globmob buildings compatibility

--- furniture/buildings.hpp
+++ furniture/buildings.hpp
@@ -170,7 +170,140 @@ _validBuildings = [
   "Land_u_Shed_Ind_F",
   "Land_VillageStore_01_F",
   "Land_Warehouse_03_F",
-  "Land_WIP_F"
+  "Land_WIP_F",
+  //Glob Mob buildings
+  "land_gm_euro_church_01",
+  "land_gm_euro_church_01_win",
+  "land_gm_euro_church_02",
+  "land_gm_euro_church_02_win",
+  "land_gm_euro_dest_01",
+  "land_gm_euro_dest_01_win",
+  "land_gm_euro_factory_01_01",
+  "land_gm_euro_factory_01_01_win",
+  "land_gm_euro_factory_01_02",
+  "land_gm_euro_factory_01_02_win",
+  "land_gm_euro_factory_02",
+  "land_gm_euro_factory_02_win",
+  "land_gm_euro_farmhouse_01",
+  "land_gm_euro_farmhouse_01_win",
+  "land_gm_euro_farmhouse_02",
+  "land_gm_euro_farmhouse_02_win",
+  "land_gm_euro_farmhouse_03",
+  "land_gm_euro_farmhouse_03_win",
+  "land_gm_euro_fuelstation_01_w",
+  "land_gm_euro_fuelstation_01_w_win",
+  "land_gm_euro_house_01_d",
+  "land_gm_euro_house_01_d_win",
+  "land_gm_euro_house_01_e",
+  "land_gm_euro_house_01_e_win",
+  "land_gm_euro_house_01_w",
+  "land_gm_euro_house_01_w_win",
+  "land_gm_euro_house_02_d",
+  "land_gm_euro_house_02_d_win",
+  "land_gm_euro_house_02_e",
+  "land_gm_euro_house_02_e_win",
+  "land_gm_euro_house_02_w",
+  "land_gm_euro_house_02_w_win",
+  "land_gm_euro_house_03_e",
+  "land_gm_euro_house_03_e_win",
+  "land_gm_euro_house_03_w",
+  "land_gm_euro_house_03_w_win",
+  "land_gm_euro_house_04_d",
+  "land_gm_euro_house_04_d_win",
+  "land_gm_euro_house_04_e",
+  "land_gm_euro_house_04_e_win",
+  "land_gm_euro_house_04_w",
+  "land_gm_euro_house_04_w_win",
+  "land_gm_euro_house_05_d",
+  "land_gm_euro_house_05_d_win",
+  "land_gm_euro_house_05_e",
+  "land_gm_euro_house_05_e_win",
+  "land_gm_euro_house_05_w",
+  "land_gm_euro_house_05_w_win",
+  "land_gm_euro_house_06_d",
+  "land_gm_euro_house_06_d_win",
+  "land_gm_euro_house_06_e",
+  "land_gm_euro_house_06_e_win",
+  "land_gm_euro_house_06_w",
+  "land_gm_euro_house_06_w_win",
+  "land_gm_euro_house_07_d",
+  "land_gm_euro_house_07_d_win",
+  "land_gm_euro_house_07_e",
+  "land_gm_euro_house_07_e_win",
+  "land_gm_euro_house_07_w",
+  "land_gm_euro_house_07_w_win",
+  "land_gm_euro_house_08_d",
+  "land_gm_euro_house_08_d_win",
+  "land_gm_euro_house_08_e",
+  "land_gm_euro_house_08_e_win",
+  "land_gm_euro_house_08_w",
+  "land_gm_euro_house_08_w_win",
+  "land_gm_euro_house_09_d",
+  "land_gm_euro_house_09_d_win",
+  "land_gm_euro_house_09_e",
+  "land_gm_euro_house_09_e_win",
+  "land_gm_euro_house_09_w",
+  "land_gm_euro_house_09_w_win",
+  "land_gm_euro_house_10_d",
+  "land_gm_euro_house_10_d_win",
+  "land_gm_euro_house_10_e",
+  "land_gm_euro_house_10_e_win",
+  "land_gm_euro_house_10_w",
+  "land_gm_euro_house_10_w_win",
+  "land_gm_euro_house_11_d",
+  "land_gm_euro_house_11_d_win",
+  "land_gm_euro_house_11_e",
+  "land_gm_euro_house_11_e_win",
+  "land_gm_euro_house_11_w",
+  "land_gm_euro_house_11_w_win",
+  "land_gm_euro_house_12_d",
+  "land_gm_euro_house_12_d_win",
+  "land_gm_euro_house_12_e",
+  "land_gm_euro_house_12_e_win",
+  "land_gm_euro_house_12_w",
+  "land_gm_euro_house_12_w_win",
+  "land_gm_euro_house_13_d",
+  "land_gm_euro_house_13_d_win",
+  "land_gm_euro_house_13_e",
+  "land_gm_euro_house_13_e_win",
+  "land_gm_euro_house_13_w",
+  "land_gm_euro_house_13_w_win",
+  "land_gm_euro_misc_garage_01_01",
+  "land_gm_euro_misc_garage_01_01_win",
+  "land_gm_euro_misc_garage_01_02",
+  "land_gm_euro_misc_garage_01_02_win",
+  "land_gm_euro_misc_garage_02",
+  "land_gm_euro_misc_garage_02_win",
+  "land_gm_euro_misc_garage_03",
+  "land_gm_euro_misc_garage_03_win",
+  "land_gm_euro_misc_garage_04",
+  "land_gm_euro_misc_garage_04_win",
+  "land_gm_euro_misc_kiosk_01",
+  "land_gm_euro_misc_kiosk_01_win",
+  "land_gm_euro_office_01",
+  "land_gm_euro_office_01_win",
+  "land_gm_euro_office_02",
+  "land_gm_euro_office_02_win",
+  "land_gm_euro_office_03",
+  "land_gm_euro_office_03_win",
+  "land_gm_euro_pub_01",
+  "land_gm_euro_pub_01_win",
+  "land_gm_euro_pub_02",
+  "land_gm_euro_pub_02_win",
+  "land_gm_euro_shed_01",
+  "land_gm_euro_shed_01_win",
+  "land_gm_euro_shed_03",
+  "land_gm_euro_shed_03_win",
+  "land_gm_euro_shed_04",
+  "land_gm_euro_shed_04_win",
+  "land_gm_euro_shed_05",
+  "land_gm_euro_shed_05_win",
+  "land_gm_euro_shop_01_w",
+  "land_gm_euro_shop_01_w_win",
+  "land_gm_euro_shop_02_e",
+  "land_gm_euro_shop_02_e_win",
+  "land_gm_euro_shop_02_w",
+  "land_gm_euro_shop_02_w_win"
 ];
 
 tint_translationNamespace = [] call CBA_fnc_createNamespace;
@@ -186,3 +319,99 @@ tint_translationNamespace setVariable ["Land_Shed_08_brown_F", "Land_Shed_08_gre
 tint_translationNamespace setVariable ["Land_Caravan_01_rust_F", "Land_Caravan_01_green_F"];
 tint_translationNamespace setVariable ["Land_Greenhouse_01_damaged_F", "Land_Greenhouse_01_F"];
 tint_translationNamespace setVariable ["Land_Chapel_V2_F", "Land_Chapel_V1_F"];
+tint_translationNamespace setVariable ["land_gm_euro_barracks_01_win", "land_gm_euro_barracks_01"];
+tint_translationNamespace setVariable ["land_gm_euro_barracks_02_win", "land_gm_euro_barracks_02"];
+tint_translationNamespace setVariable ["land_gm_euro_church_01_win", "land_gm_euro_church_01"];
+tint_translationNamespace setVariable ["land_gm_euro_church_02_win", "land_gm_euro_church_02"];
+tint_translationNamespace setVariable ["land_gm_euro_dest_01_win", "land_gm_euro_dest_01"];
+tint_translationNamespace setVariable ["land_gm_euro_factory_01_01_win", "land_gm_euro_factory_01_01"];
+tint_translationNamespace setVariable ["land_gm_euro_factory_01_02", "land_gm_euro_factory_01_01"];
+tint_translationNamespace setVariable ["land_gm_euro_factory_01_02_win", "land_gm_euro_factory_01_01"];
+tint_translationNamespace setVariable ["land_gm_euro_factory_02_win", "land_gm_euro_factory_02"];
+tint_translationNamespace setVariable ["land_gm_euro_farmhouse_01_win", "land_gm_euro_farmhouse_01"];
+tint_translationNamespace setVariable ["land_gm_euro_farmhouse_02_win", "land_gm_euro_farmhouse_02"];
+tint_translationNamespace setVariable ["land_gm_euro_farmhouse_03_win", "land_gm_euro_farmhouse_03"];
+tint_translationNamespace setVariable ["land_gm_euro_fuelstation_01_w_win", "land_gm_euro_fuelstation_01_w"];
+tint_translationNamespace setVariable ["land_gm_euro_house_01_d_win", "land_gm_euro_house_01_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_01_e", "land_gm_euro_house_01_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_01_e_win", "land_gm_euro_house_01_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_01_w", "land_gm_euro_house_01_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_01_w_win", "land_gm_euro_house_01_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_02_d_win", "land_gm_euro_house_02_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_02_e", "land_gm_euro_house_02_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_02_e_win", "land_gm_euro_house_02_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_02_w", "land_gm_euro_house_02_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_02_w_win", "land_gm_euro_house_02_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_03_e_win", "land_gm_euro_house_03_e"];
+tint_translationNamespace setVariable ["land_gm_euro_house_03_w", "land_gm_euro_house_03_e"];
+tint_translationNamespace setVariable ["land_gm_euro_house_03_w_win", "land_gm_euro_house_03_e"];
+tint_translationNamespace setVariable ["land_gm_euro_house_04_d_win", "land_gm_euro_house_04_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_04_e", "land_gm_euro_house_04_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_04_e_win", "land_gm_euro_house_04_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_04_w", "land_gm_euro_house_04_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_04_w_win", "land_gm_euro_house_04_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_05_d_win", "land_gm_euro_house_05_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_05_e", "land_gm_euro_house_05_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_05_e_win", "land_gm_euro_house_05_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_05_w", "land_gm_euro_house_05_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_05_w_win", "land_gm_euro_house_05_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_06_d_win", "land_gm_euro_house_06_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_06_e", "land_gm_euro_house_06_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_06_e_win", "land_gm_euro_house_06_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_06_w", "land_gm_euro_house_06_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_06_w_win", "land_gm_euro_house_06_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_07_d_win", "land_gm_euro_house_07_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_07_e", "land_gm_euro_house_07_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_07_e_win", "land_gm_euro_house_07_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_07_w", "land_gm_euro_house_07_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_07_w_win", "land_gm_euro_house_07_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_08_d_win", "land_gm_euro_house_08_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_08_e", "land_gm_euro_house_08_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_08_e_win", "land_gm_euro_house_08_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_08_w", "land_gm_euro_house_08_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_08_w_win", "land_gm_euro_house_08_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_09_d_win", "land_gm_euro_house_09_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_09_e", "land_gm_euro_house_09_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_09_e_win", "land_gm_euro_house_09_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_09_w", "land_gm_euro_house_09_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_09_w_win", "land_gm_euro_house_09_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_10_d_win", "land_gm_euro_house_10_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_10_e", "land_gm_euro_house_10_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_10_e_win", "land_gm_euro_house_10_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_10_w", "land_gm_euro_house_10_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_10_w_win", "land_gm_euro_house_10_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_11_d_win", "land_gm_euro_house_11_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_11_e", "land_gm_euro_house_11_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_11_e_win", "land_gm_euro_house_11_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_11_w", "land_gm_euro_house_11_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_11_w_win", "land_gm_euro_house_11_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_12_d_win", "land_gm_euro_house_12_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_12_e", "land_gm_euro_house_12_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_12_e_win", "land_gm_euro_house_12_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_12_w", "land_gm_euro_house_12_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_12_w_win", "land_gm_euro_house_12_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_13_d_win", "land_gm_euro_house_13_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_13_e", "land_gm_euro_house_13_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_13_e_win", "land_gm_euro_house_13_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_13_w", "land_gm_euro_house_13_d"];
+tint_translationNamespace setVariable ["land_gm_euro_house_13_w_win", "land_gm_euro_house_13_d"];
+tint_translationNamespace setVariable ["land_gm_euro_misc_garage_01_01_win", "land_gm_euro_misc_garage_01_01"];
+tint_translationNamespace setVariable ["land_gm_euro_misc_garage_01_02", "land_gm_euro_misc_garage_01_01"];
+tint_translationNamespace setVariable ["land_gm_euro_misc_garage_01_02_win", "land_gm_euro_misc_garage_01_01"];
+tint_translationNamespace setVariable ["land_gm_euro_misc_garage_02_win", "land_gm_euro_misc_garage_02"];
+tint_translationNamespace setVariable ["land_gm_euro_misc_garage_03_win", "land_gm_euro_misc_garage_03"];
+tint_translationNamespace setVariable ["land_gm_euro_misc_garage_04_win", "land_gm_euro_misc_garage_04"];
+tint_translationNamespace setVariable ["land_gm_euro_misc_kiosk_01_win", "land_gm_euro_misc_kiosk_01"];
+tint_translationNamespace setVariable ["land_gm_euro_office_01_win", "land_gm_euro_office_01"];
+tint_translationNamespace setVariable ["land_gm_euro_office_02_win", "land_gm_euro_office_02"];
+tint_translationNamespace setVariable ["land_gm_euro_office_03_win", "land_gm_euro_office_03"];
+tint_translationNamespace setVariable ["land_gm_euro_pub_01_win", "land_gm_euro_pub_01"];
+tint_translationNamespace setVariable ["land_gm_euro_pub_02_win", "land_gm_euro_pub_02"];
+tint_translationNamespace setVariable ["land_gm_euro_shed_01_win", "land_gm_euro_shed_01"];
+tint_translationNamespace setVariable ["land_gm_euro_shed_03_win", "land_gm_euro_shed_03"];
+tint_translationNamespace setVariable ["land_gm_euro_shed_04_win", "land_gm_euro_shed_04"];
+tint_translationNamespace setVariable ["land_gm_euro_shed_05_win", "land_gm_euro_shed_05"];
+tint_translationNamespace setVariable ["land_gm_euro_shop_01_w_win", "land_gm_euro_shop_01_w"];
+tint_translationNamespace setVariable ["land_gm_euro_shop_02_e_win", "land_gm_euro_shop_02_e"];
+tint_translationNamespace setVariable ["land_gm_euro_shop_02_w", "land_gm_euro_shop_02_e"];
+tint_translationNamespace setVariable ["land_gm_euro_shop_02_w_win", "land_gm_euro_shop_02_e"];
