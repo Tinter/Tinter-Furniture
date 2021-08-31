b57f45e05caad83425574b23fe12790bc7747ed9
Tinter <a-348@hotmail.com>
2021-08-31 21:57:01 +0200

    VN buildings added

--- furniture/buildings.hpp
+++ furniture/buildings.hpp
@@ -35,6 +35,7 @@ _validBuildings = [
   "Land_Airport_02_terminal_F",
   "Land_Airport_Tower_F",
   "Land_Addon_03mid_V1_ruins_F",
+  "Land_Addon_03mid_V1_F",
   "Land_Barn_01_brown_F",
   "Land_Barn_01_brown_F",
   "Land_Barn_01_grey_F",
@@ -377,7 +378,170 @@ _validBuildings = [
   "Land_WL_House_01_A",
   "Land_WL_House_02_A",
   "Land_WL_House_03_A",
-  "Land_WL_House_04_A"
+  "Land_WL_House_04_A",
+  //Unique Cam Lao Nam buildings
+  "Land_vn_ind_vysypka",
+  "Land_vn_army_hut_int",
+  "Land_vn_army_hut2_int",
+  "Land_vn_army_hut3_long_int",
+  "Land_vn_army_hut_storrage",
+  "Land_vn_hlaska",
+  "Land_vn_quonset_01",
+  "Land_vn_bar_01",
+  "Land_vn_mil_barracks_i_ep1",
+  "Land_vn_radar_01_hq_f",
+  "Land_vn_strazni_vez",
+  "Land_vn_hut_mont_02",
+  "Land_vn_hut_mont_03",
+  "Land_vn_hut_mont_04",
+  "Land_vn_hut_mont_05",
+  "Land_vn_hut_mont_01",
+  "Land_vn_hut_river_02",
+  "Land_vn_hut_river_03",
+  "Land_vn_hut_river_01",
+  "Land_vn_a_hospital",
+  "Land_vn_houseb_tenement",
+  "Land_vn_a_office01",
+  "Land_vn_house_c_1_v2_ep1",
+  "Land_vn_house_c_3_ep1",
+  "Land_vn_house_c_9_ep1",
+  "Land_vn_house_c_11_ep1",
+  "Land_vn_house_c_12_ep1",
+  "Land_vn_housev2_04_interier",
+  "Land_vn_slum_03_01_f",
+  "Land_vn_barracks_02",
+  "Land_vn_barracks_02_01",
+  "Land_vn_barracks_03_01",
+  "Land_vn_barracks_03_02",
+  "Land_vn_barracks_03_03",
+  "Land_vn_barracks_03_04",
+  "Land_vn_barracks_04_01",
+  "Land_vn_barracks_04_02",
+  "Land_vn_barracks_04",
+  "Land_vn_barracks_01",
+  "Land_vn_barracks_03",
+  "Land_vn_barracks_01_camo_f",
+  "Land_vn_guardhouse_03",
+  "Land_vn_guardhouse_02",
+  "Land_vn_guardhouse_01",
+  "Land_vn_hootch_01_01",
+  "Land_vn_hootch_01_02",
+  "Land_vn_hootch_01_03",
+  "Land_vn_hootch_01_11",
+  "Land_vn_hootch_01_12",
+  "Land_vn_hootch_01_13",
+  "Land_vn_hootch_02_01",
+  "Land_vn_hootch_02_02",
+  "Land_vn_hootch_02_03",
+  "Land_vn_hootch_02_11",
+  "Land_vn_hootch_02",
+  "Land_vn_hootch_01",
+  "Land_vn_hut_tower_02",
+  "Land_vn_hut_tower_03",
+  "Land_vn_hut_tower_01",
+  "Land_vn_hut_village_01",
+  "Land_vn_hut_village_02",
+  "Land_vn_hut_02",
+  "Land_vn_hut_03",
+  "Land_vn_hut_04",
+  "Land_vn_hut_05",
+  "Land_vn_hut_06",
+  "Land_vn_hut_07",
+  "Land_vn_hut_08",
+  "Land_vn_hut_01",
+  "Land_vn_quonset_02_01",
+  "Land_vn_school_01_01_f",
+  "Land_vn_tent_01_02",
+  "Land_vn_tent_01_03",
+  "Land_vn_tent_01_04",
+  "Land_vn_tent_02_01",
+  "Land_vn_tent_02_02",
+  "Land_vn_tent_02_03",
+  "Land_vn_tent_02_04",
+  "Land_vn_tent_01_01",
+  "Land_vn_tent_mash_01",
+  "Land_vn_tent_mash_01_01",
+  "Land_vn_tent_mash_01_02",
+  "Land_vn_tent_mash_01_03",
+  "Land_vn_tent_mash_01_04",
+  "Land_vn_tent_mash_02_01",
+  "Land_vn_tent_mash_02_02",
+  "Land_vn_tent_mash_02_03",
+  "Land_vn_tent_mash_02_04",
+  "Land_vn_latrine_01",
+  //Generic Cam Lao Nam buildings, thank you for copying all the buildings...
+  "Land_vn_airport_01_controltower_f",
+  "Land_vn_airport_02_controltower_f",
+  "Land_vn_airport_02_terminal_f",
+  "Land_vn_airport_01_terminal_f",
+  "Land_vn_airport_01_hangar_f",
+  "Land_vn_cementworks_01_base_f",
+  "Land_vn_factory_02_f",
+  "Land_vn_garagerow_01_large_f",
+  "Land_vn_i_garage_v1_f",
+  "Land_vn_garagerow_01_small_f",
+  "Land_vn_garageoffice_01_f",
+  "Land_vn_metal_shed_f",
+  "Land_vn_u_addon_02_v1_f",
+  "Land_vn_i_shed_ind_f",
+  "Land_vn_sm_01_shed_f",
+  "Land_vn_i_addon_03_v1_f",
+  "Land_vn_i_addon_03mid_v1_f",
+  "Land_vn_i_addon_04_v1_f",
+  "Land_vn_u_addon_01_v1_f",
+  "Land_vn_industrialshed_01_f",
+  "Land_vn_i_shed_ind_old_f",
+  "Land_vn_warehouse_03_f",
+  "Land_vn_workshop_05_f",
+  "Land_vn_workshop_03_f",
+  "Land_vn_workshop_04_f",
+  "Land_vn_workshop_02_f",
+  "Land_vn_workshop_01_f",
+  "Land_vn_i_barracks_v1_f",
+  "Land_vn_barracks_01_dilapidated_f",
+  "Land_vn_barracks_06_f",
+  "Land_vn_i_barracks_v1_f",
+  "Land_vn_barracks_02_f",
+  "Land_vn_barracks_03_f",
+  "Land_vn_barracks_04_f",
+  "Land_vn_barracks_05_f",
+  "Land_vn_controltower_01_f",
+  "Land_vn_guardbox_01_base_f",
+  "Land_vn_guardhouse_02_f",
+  "Land_vn_guardhouse_03_f",
+  "Land_vn_guardtower_01_f",
+  "Land_vn_radar_01_kitchen_f",
+  "Land_vn_gh_house_2_f",
+  "Land_vn_shop_town_01_f", 
+  "Land_vn_house_small_01_f",
+  "Land_vn_house_small_02_f",
+  "Land_vn_house_small_03_f",
+  "Land_vn_house_small_04_f",
+  "Land_vn_house_small_05_f",
+  "Land_vn_house_small_06_f",
+  "Land_vn_house_big_01_f",
+  "Land_vn_house_big_02_f",
+  "Land_vn_house_big_03_f",
+  "Land_vn_house_big_04_f",
+  "Land_vn_shop_city_01_f",
+  "Land_vn_shop_city_02_f",
+  "Land_vn_shed_02_f",
+  "Land_vn_slum_01_f",
+  "Land_vn_slum_02_f",
+  "Land_vn_slum_03_f",
+  "Land_vn_hotel_01_f",
+  "Land_vn_hotel_02_f",
+  "Land_vn_garageshelter_01_f",
+  "Land_vn_shop_city_03_f",
+  "Land_vn_shop_city_06_f",
+  "Land_vn_school_01_f",
+  "Land_vn_slum_house03_f",
+  "Land_vn_slum_house01_f",
+  "Land_vn_slum_house02_f",
+  "Land_vn_supermarket_01_f",
+  "Land_vn_unfinished_building_01_f",
+  "Land_vn_unfinished_building_02_f",
+  "Land_vn_wip_f"
 ];
 
 tint_translationNamespace = [] call CBA_fnc_createNamespace;
@@ -393,6 +557,7 @@ tint_translationNamespace setVariable ["Land_Shed_08_brown_F", "Land_Shed_08_gre
 tint_translationNamespace setVariable ["Land_Caravan_01_rust_F", "Land_Caravan_01_green_F"];
 tint_translationNamespace setVariable ["Land_Greenhouse_01_damaged_F", "Land_Greenhouse_01_F"];
 tint_translationNamespace setVariable ["Land_i_Shed_Ind_old_F", "Land_i_Shed_Ind_F"];
+tint_translationNamespace setVariable ["Land_SM_01_shed_F", "Land_i_Shed_Ind_F"];
 tint_translationNamespace setVariable ["Land_Chapel_V2_F", "Land_Chapel_V1_F"];
 tint_translationNamespace setVariable ["Land_Bunker_02_light_double_F", "Land_Bunker_02_double_F"];
 tint_translationNamespace setVariable ["Land_Bunker_02_light_left_F", "Land_Bunker_02_left_F"];
@@ -504,3 +669,88 @@ tint_translationNamespace setVariable ["land_gm_euro_shop_01_w_win", "land_gm_eu
 tint_translationNamespace setVariable ["land_gm_euro_shop_02_e_win", "land_gm_euro_shop_02_e"];
 tint_translationNamespace setVariable ["land_gm_euro_shop_02_w", "land_gm_euro_shop_02_e"];
 tint_translationNamespace setVariable ["land_gm_euro_shop_02_w_win", "land_gm_euro_shop_02_e"];
+tint_translationNamespace setVariable ["Land_vn_airport_01_controltower_f", "Land_Airport_01_controlTower_F"];
+tint_translationNamespace setVariable ["Land_vn_airport_02_controltower_f", "Land_airport_02_controltower_f"];
+tint_translationNamespace setVariable ["Land_vn_airport_02_terminal_f", "Land_airport_02_terminal_f"];
+tint_translationNamespace setVariable ["Land_vn_airport_01_terminal_f", "Land_airport_01_terminal_f"];
+tint_translationNamespace setVariable ["Land_vn_airport_01_hangar_f", "Land_airport_01_hangar_f"];
+tint_translationNamespace setVariable ["Land_vn_cementworks_01_base_f", "Land_CementWorks_01_base_F"];
+tint_translationNamespace setVariable ["Land_vn_factory_02_f", "Land_factory_02_f"];
+tint_translationNamespace setVariable ["Land_vn_garagerow_01_large_f", "Land_GarageRow_01_large_F"];
+tint_translationNamespace setVariable ["Land_vn_i_garage_v1_f", "Land_i_Garage_V1_F"];
+tint_translationNamespace setVariable ["Land_vn_garagerow_01_small_f", "Land_GarageRow_01_small_F"];
+tint_translationNamespace setVariable ["Land_vn_garageoffice_01_f", "Land_GarageOffice_01_F"];
+tint_translationNamespace setVariable ["Land_vn_metal_shed_f", "Land_Metal_Shed_F"];
+tint_translationNamespace setVariable ["Land_vn_u_addon_02_v1_f", "Land_u_Addon_02_V1_F"];
+tint_translationNamespace setVariable ["Land_vn_i_shed_ind_f", "Land_i_Shed_Ind_F"];
+tint_translationNamespace setVariable ["Land_vn_sm_01_shed_f", "Land_i_Shed_Ind_F"];
+tint_translationNamespace setVariable ["Land_vn_i_addon_03_v1_f", "Land_i_Addon_03_V1_F"];
+tint_translationNamespace setVariable ["Land_vn_i_addon_03mid_v1_f", "Land_Addon_03mid_V1_F"];
+tint_translationNamespace setVariable ["Land_vn_i_addon_04_v1_f", "Land_i_Addon_04_V1_F"];
+tint_translationNamespace setVariable ["Land_vn_u_addon_01_v1_f", "Land_u_Addon_01_V1_F"];
+tint_translationNamespace setVariable ["Land_vn_industrialshed_01_f", "Land_IndustrialShed_01_F"];
+tint_translationNamespace setVariable ["Land_vn_i_shed_ind_old_f", "Land_i_Shed_Ind_F"];
+tint_translationNamespace setVariable ["Land_vn_warehouse_03_f", "Land_Warehouse_03_F"];
+tint_translationNamespace setVariable ["Land_vn_workshop_05_f", "Land_Workshop_05_F"];
+tint_translationNamespace setVariable ["Land_vn_workshop_03_f", "Land_Workshop_03_F"];
+tint_translationNamespace setVariable ["Land_vn_workshop_04_f", "Land_Workshop_04_F"];
+tint_translationNamespace setVariable ["Land_vn_workshop_02_f", "Land_Workshop_02_F"];
+tint_translationNamespace setVariable ["Land_vn_workshop_01_f", "Land_Workshop_01_F"];
+tint_translationNamespace setVariable ["Land_vn_i_barracks_v1_f", "Land_i_Barracks_V1_F"];
+tint_translationNamespace setVariable ["Land_vn_barracks_01_dilapidated_f", "Land_i_Barracks_V1_F"];
+tint_translationNamespace setVariable ["Land_vn_barracks_06_f", "Land_Barracks_06_F"];
+tint_translationNamespace setVariable ["Land_vn_i_barracks_v1_f", "Land_i_Barracks_V1_F"];
+tint_translationNamespace setVariable ["Land_vn_barracks_02_f", "Land_Barracks_02_F"];
+tint_translationNamespace setVariable ["Land_vn_barracks_03_f", "Land_Barracks_03_F"];
+tint_translationNamespace setVariable ["Land_vn_barracks_04_f", "Land_Barracks_04_F"];
+tint_translationNamespace setVariable ["Land_vn_barracks_05_f", "Land_Barracks_05_F"];
+tint_translationNamespace setVariable ["Land_vn_controltower_01_f", "Land_ControlTower_01_F"];
+tint_translationNamespace setVariable ["Land_vn_guardbox_01_base_f", "Land_GuardBox_01_base_F"];
+tint_translationNamespace setVariable ["Land_vn_guardhouse_02_f", "Land_GuardHouse_02_F"];
+tint_translationNamespace setVariable ["Land_vn_guardhouse_03_f", "Land_GuardHouse_03_F"];
+tint_translationNamespace setVariable ["Land_vn_guardtower_01_f", "Land_GuardTower_01_F"];
+tint_translationNamespace setVariable ["Land_vn_radar_01_kitchen_f", "Land_Radar_01_kitchen_F"];
+tint_translationNamespace setVariable ["Land_vn_gh_house_2_f", "Land_GH_House_2_F"];
+tint_translationNamespace setVariable ["Land_vn_shop_town_01_f", "Land_Shop_Town_01_F" ];
+tint_translationNamespace setVariable ["Land_vn_house_small_01_f", "Land_House_Small_01_F"];
+tint_translationNamespace setVariable ["Land_vn_house_small_02_f", "Land_House_Small_02_F"];
+tint_translationNamespace setVariable ["Land_vn_house_small_03_f", "Land_House_Small_03_F"];
+tint_translationNamespace setVariable ["Land_vn_house_small_04_f", "Land_House_Small_04_F"];
+tint_translationNamespace setVariable ["Land_vn_house_small_05_f", "Land_House_Small_05_F"];
+tint_translationNamespace setVariable ["Land_vn_house_small_06_f", "Land_House_Small_06_F"];
+tint_translationNamespace setVariable ["Land_vn_house_big_01_f", "Land_House_Big_01_F"];
+tint_translationNamespace setVariable ["Land_vn_house_big_02_f", "Land_House_Big_02_F"];
+tint_translationNamespace setVariable ["Land_vn_house_big_03_f", "Land_House_Big_03_F"];
+tint_translationNamespace setVariable ["Land_vn_house_big_04_f", "Land_House_Big_04_F"];
+tint_translationNamespace setVariable ["Land_vn_shop_city_01_f", "Land_Shop_City_01_F"];
+tint_translationNamespace setVariable ["Land_vn_shop_city_02_f", "Land_Shop_City_02_F"];
+tint_translationNamespace setVariable ["Land_vn_shed_02_f", "Land_Shed_02_F"];
+tint_translationNamespace setVariable ["Land_vn_slum_01_f", "Land_Slum_01_F"];
+tint_translationNamespace setVariable ["Land_vn_slum_02_f", "Land_Slum_02_F"];
+tint_translationNamespace setVariable ["Land_vn_slum_03_f", "Land_Slum_03_F"];
+tint_translationNamespace setVariable ["Land_vn_hotel_01_f", "Land_Hotel_01_F"];
+tint_translationNamespace setVariable ["Land_vn_hotel_02_f", "Land_Hotel_02_F"];
+tint_translationNamespace setVariable ["Land_vn_garageshelter_01_f", "Land_GarageShelter_01_F"];
+tint_translationNamespace setVariable ["Land_vn_shop_city_03_f", "Land_Shop_City_03_F"];
+tint_translationNamespace setVariable ["Land_vn_shop_city_06_f", "Land_Shop_City_06_F"];
+tint_translationNamespace setVariable ["Land_vn_school_01_f", "Land_School_01_F"];
+tint_translationNamespace setVariable ["Land_vn_slum_house03_f", "Land_Slum_House03_F"];
+tint_translationNamespace setVariable ["Land_vn_slum_house01_f", "Land_Slum_House01_F"];
+tint_translationNamespace setVariable ["Land_vn_slum_house02_f", "Land_Slum_House02_F"];
+tint_translationNamespace setVariable ["Land_vn_supermarket_01_f", "Land_Supermarket_01_malden_F"];
+tint_translationNamespace setVariable ["Land_vn_unfinished_building_01_f", "Land_Unfinished_Building_01_F"];
+tint_translationNamespace setVariable ["Land_vn_unfinished_building_02_f", "Land_Unfinished_Building_02_F"];
+tint_translationNamespace setVariable ["Land_vn_wip_f", "Land_WIP_F"];
+tint_translationNamespace setVariable ["Land_vn_hootch_01_11", "Land_vn_hootch_01_01"];
+tint_translationNamespace setVariable ["Land_vn_hootch_01_12", "Land_vn_hootch_01_02"];
+tint_translationNamespace setVariable ["Land_vn_hootch_01_13", "Land_vn_hootch_01_03"];
+tint_translationNamespace setVariable ["Land_vn_hootch_02_11", "Land_vn_hootch_02_01"];
+tint_translationNamespace setVariable ["Land_vn_tent_01_02", "Land_vn_tent_01_01"];
+tint_translationNamespace setVariable ["Land_vn_tent_01_04", "Land_vn_tent_01_03"];
+tint_translationNamespace setVariable ["Land_vn_tent_02_02", "Land_vn_tent_02_01"];
+tint_translationNamespace setVariable ["Land_vn_tent_02_04", "Land_vn_tent_02_03  "];
+tint_translationNamespace setVariable ["Land_vn_tent_mash_01_01", "Land_vn_tent_mash_01"];
+tint_translationNamespace setVariable ["Land_vn_tent_mash_01_02", "Land_vn_tent_mash_01"];
+tint_translationNamespace setVariable ["Land_vn_tent_mash_01_04", "Land_vn_tent_mash_01_03"];
+tint_translationNamespace setVariable ["Land_vn_tent_mash_02_02", "Land_vn_tent_mash_02_01"];
+tint_translationNamespace setVariable ["Land_vn_tent_mash_02_04", "Land_vn_tent_mash_02_03"];
